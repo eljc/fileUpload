af104c39b7abc277f4abe18fc084fde477e12593
Elder Camara <elder.camara@cidadania.gov.br>
2022-05-14 06:06:51 -0300

    test project

--- src/main/java/br/com/eljc/upload/FileUploadApplication.java
+++ src/main/java/br/com/eljc/upload/FileUploadApplication.java
@@ -2,8 +2,12 @@ package br.com.eljc.upload;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import br.com.eljc.upload.config.StorageProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class FileUploadApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/br/com/eljc/upload/exception/StorageFileNotFoundException.java
@@ -0,0 +1,13 @@
+package br.com.eljc.upload.exception;
+
+public class StorageFileNotFoundException extends StorageException {
+
+	public StorageFileNotFoundException(String message) {
+		super(message);
+	}
+
+	public StorageFileNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+}
